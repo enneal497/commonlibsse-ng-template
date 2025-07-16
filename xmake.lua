98ddf34b174178c2524a626a7b6d2a35f662308b
enneal497 <enneal497@gmail.com>
2025-07-16 17:52:04 +0100

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -3,6 +3,7 @@ set_xmakever("2.8.2")
 
 -- includes
 includes("lib/commonlibsse-ng")
+includes("lib/CLibUtil")
 
 -- set project
 set_project("commonlibsse-ng-template")
@@ -12,6 +13,7 @@ set_license("GPL-3.0")
 -- set defaults
 set_languages("c++23")
 set_warnings("allextra")
+set_defaultmode("releasedbg")
 
 -- set policies
 set_policy("package.requires_lock", true)
@@ -20,20 +22,26 @@ set_policy("package.requires_lock", true)
 add_rules("mode.debug", "mode.releasedbg")
 add_rules("plugin.vsxmake.autoupdate")
 
+-- require packages
+add_requires("xbyak", "simpleini")
+
 -- targets
 target("commonlibsse-ng-template")
     -- add dependencies to target
-    add_deps("commonlibsse-ng")
+    add_deps("commonlibsse-ng", "CLibUtil")
+
+    -- add packages to target
+    add_packages("xbyak", "simpleini")
 
     -- add commonlibsse-ng plugin
     add_rules("commonlibsse-ng.plugin", {
         name = "commonlibsse-ng-template",
-        author = "qudix",
+        author = "Enneal",
         description = "SKSE64 plugin template using CommonLibSSE-NG"
     })
 
     -- add src files
     add_files("src/**.cpp")
-    add_headerfiles("src/**.h")
-    add_includedirs("src")
-    set_pcxxheader("src/pch.h")
+    add_headerfiles("include/**.h")
+    add_includedirs("include")
+    set_pcxxheader("include/pch.h")
