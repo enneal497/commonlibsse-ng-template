56cf22e1d441dd5978e1c41d64be716d03722559
enneal497 <enneal497@gmail.com>
2025-07-20 16:54:55 +0100

    Update Settings.cpp

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -11,11 +11,6 @@ namespace Settings
         ini.LoadFile(R"(.\Data\SKSE\Plugins\__plugin_name__.ini)");
 
         //Load global settings
-        clib_util::ini::get_value(ini, debug_logging, "General", "bDebug");
-        if (debug_logging) {
-            spdlog::set_level(spdlog::level::debug);
-            logger::debug("Debug logging enabled");
-        }
 
         logger::info("Loaded settings");
         logger::info("");
